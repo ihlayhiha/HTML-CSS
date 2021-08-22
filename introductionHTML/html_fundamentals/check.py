fd5cb9e5b029a11cd674c761186c134fa55fb812
Teja Linux <raviteja.yella@gmail.com>
2021-08-22 16:17:19 +0530

    added a basic css stylesheet for index.html

---
+++ introductionHTML/html_fundamentals/check.py
@@ -0,0 +1,16 @@
+this = 10
+print(this)
+
+class whater :
+    def __init__(self, a , b) -> None:
+        # whatever this class is
+        def function(list):
+            # takes a list and  gives u mback notjhing
+            a= 10
+            b = 20
+            while a < b:
+                print("Just checking to see whatevr sticks")
+            
+        self.a = a
+        self.b = b
+        does this autosave?
